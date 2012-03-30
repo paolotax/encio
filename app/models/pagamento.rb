7eed9332dfc5cf091963c7933a007fad52cb4341
paolotax <paolo.tassinari@gmail.com>
2012-03-30 15:27:35 +0200

    ordinestaff

--- app/models/pagamento.rb
+++ app/models/pagamento.rb
@@ -1,4 +1,8 @@
 class Pagamento < ActiveRecord::Base
   belongs_to :serata
   belongs_to :dipendente
+  
+  default_scope includes(:dipendente => [:ruolo]).order('dipendenti.ruolo_id')
+  
+  scope :staff_ordine, includes(:dipendente => [:ruolo]).order('ruolo.nome asc')
 end
