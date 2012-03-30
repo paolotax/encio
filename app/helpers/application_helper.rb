267cc0d17d5601d1a6cccabd46e121fe929b39e9
paolotax <paolo.tassinari@gmail.com>
2012-03-30 14:44:34 +0200

    pretty

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+# encoding: utf-8
+
 module ApplicationHelper
+  
+  def pretty_prezzo( prezzo, *args )
+    content_tag :div, class: 'pretty-prezzo' do
+      content_tag :strong do
+        raw(
+          "€ #{prezzo.to_i.to_s}," +
+          content_tag( :sup, ((prezzo - prezzo.to_i) * 100).to_i.to_s.ljust(2, '0')  )
+        )
+      end  
+    end
+  end
+
 end
